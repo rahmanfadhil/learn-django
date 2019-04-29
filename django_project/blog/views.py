56eb381a68d4f3855855f89773e6e088376d0d50
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-29 20:05:16 +0700

    Render blog posts through context

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'title': 'Blog Post 1',
+        'content': 'First blog post content',
+        'author': 'Rahman Fadhil',
+        'date_posted': 'August 27, 2019'
+    },
+    {
+        'title': 'Blog Post 2',
+        'content': 'Second blog post content',
+        'author': 'Rahman Fadhil',
+        'date_posted': 'August 28, 2019'
+    },
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context=context)
 
 
 def about(request):
