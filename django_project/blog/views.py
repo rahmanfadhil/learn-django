ee793b418e20e5536c11d6f73a1e8e4b7e2d754b
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-29 20:00:43 +0700

    Install blog app and render from templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Home page</h1>')
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render(request, 'blog/about.html')
