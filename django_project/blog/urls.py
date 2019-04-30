d86b3dec2e7863184111bececf2107937c227da9
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-30 11:31:51 +0700

    Add post create view

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, about
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    about
+)
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', about, name='blog-about')
 ]
