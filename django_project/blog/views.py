65a0780fc96a2db2e09f68c57f470eae12f6b0b3
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-30 11:54:21 +0700

    Add user passes test mixin in update post view

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -6,7 +6,10 @@ from django.views.generic import (
     CreateView,
     UpdateView
 )
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 
 from .models import Post
 
@@ -36,13 +39,20 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
 
 
 def about(request):
